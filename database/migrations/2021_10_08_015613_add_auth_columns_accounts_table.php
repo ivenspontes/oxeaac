fcaf971ff655644e6af8277b40c792496c91b077
Ivens Pontes <ivenscardoso@hotmail.com>
2021-10-08 23:29:36 -0300

    add Accounts

--- database/migrations/2021_10_08_015613_add_auth_columns_accounts_table.php
+++ database/migrations/2021_10_08_015613_add_auth_columns_accounts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class AddAuthColumnsAccountsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+        Schema::table('accounts', function (Blueprint $table) {
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 60)->change();
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
@@ -31,6 +27,9 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->string('password', 40)->change();
+            $table->dropColumn(['email_verified_at', 'remember_token']);
+        });
     }
 }
